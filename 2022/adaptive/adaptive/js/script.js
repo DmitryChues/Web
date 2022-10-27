ba989f665d20416fe0e1631c97f95c413744dc02
DmitryChues <dmitrychues2312@gmail.com>
2022-10-27 13:41:57 +0300

    adaptive

---
+++ 2022/adaptive/adaptive/js/script.js
@@ -0,0 +1,18 @@
+document.querySelector('.wrapper').classList.add('loaded');
+document.querySelector('.icon-menu').click(function (event) {
+	document.querySelector(this).classList.toggle('active');
+	document.querySelector('.menu__body').classList.toggle('active');
+	document.querySelector('body').classList.toggle('lock');
+});
+
+function ibg() {
+
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+
+ibg();
